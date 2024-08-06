506811d38d488e63010f0d390aa6e52528f2d911
User <User@DESKTOP-RG04IRQ>
2024-08-06 14:48:18 +0530

    first push

---
+++ selenium_jenkins/src/test/java/jenkins/DemoTest.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+@Test 
+	
+	public void sample() 
+	{
+		System.out.println("DemoTest executed in jenkins");
+	}
+
+}
